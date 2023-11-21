95409eb27347169d5b3731d680a05055acc69315
葡糖糖 <madifeng21@mails.ucas.ac.cn>
2023-11-21 12:44:13 +0800

    A loader test!

--- loader.py
+++ loader.py
@@ -2,9 +2,12 @@ import pickle
 import pandas as pd
 DATA_FILE_PATH = "./DataSet/"
 
-# 打开并加载pkl文件
-with open(DATA_FILE_PATH+'text_features.pkl', 'rb') as f:
+with open(DATA_FILE_PATH + 'text_features.pkl', 'rb') as f:
     text_features = pickle.load(f)
 
-# 打印加载的数据
-print(text_features)
+# 提取以"dialog+数字ID"为键的二维数组
+dialog_ID_features = {k: v for k, v in text_features.items() if k.startswith('dialog_') and k[7:].isdigit()}
+
+# 打印提取的数据
+for key, value in dialog_ID_features.items():
+    print(f"{key}: {value}")
