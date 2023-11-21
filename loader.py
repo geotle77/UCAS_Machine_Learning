548284c80766e086100eb840102caa25243a328b
葡糖糖 <madifeng21@mails.ucas.ac.cn>
2023-11-21 12:27:12 +0800

    A loader test!

--- loader.py
+++ loader.py
@@ -1,4 +1,10 @@
 import pickle
 import pandas as pd
-DATA_FILE_PATH = "./DataSet"
+DATA_FILE_PATH = "./DataSet/"
 
+# 打开并加载pkl文件
+with open(DATA_FILE_PATH+'text_features.pkl', 'rb') as f:
+    text_features = pickle.load(f)
+
+# 打印加载的数据
+print(text_features)
